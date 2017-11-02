f5314f9f0499bbcc9f693e8eee4c5c3f60533825
janderson.rosa <janderson_raffael@hotmail.com>
2017-11-01 22:02:11 -0200

    import css

--- client/app.js
+++ client/app.js
@@ -1,4 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'jquery/dist/jquery.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css'
 
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
@@ -7,5 +9,6 @@ import uirouter from 'angular-ui-router';
 import routes from './routes';
 import components from './components';
 // angular.module('app', [index]);
+
 angular.module('app', [uirouter, components])
   .config(routes);
